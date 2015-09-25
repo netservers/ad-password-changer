5baf42f0b74e38879762962c699508195db295ad
John McEleney <john.mceleney@netservers.co.uk>
2015-09-25 14:00:59 +0100

    Fetch config from environment

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 #
 # Copyright (c) 2014 Tirith
+# Copyright (c) 2015 Netsevers Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License")
 #
@@ -14,13 +15,13 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DEBUG = False
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 # App name
-APP_NAME = 'AD password changer'
+APP_NAME = os.environ['APP_NAME'] if 'APP_NAME' in os.environ else 'AD password changer'
 # App host
 SERVER_HOST = '0.0.0.0'
 # App port
 SERVER_PORT = 7777
 # Secret key for signing cookies
 # echo '<your_passphrase>' | md5
-SECRET_KEY = 'a4a6cb8b60695d718a902afaba4c2765'
+SECRET_KEY = os.environ['SECRET_KEY'] if 'SECRET_KEY' in os.environ else 'de7d72d42dc64185bfc7d37b911fac65'
 # Ad Server
-AD_SERVER = 'PUT YOUR DOMAIN CONTROLLER SERVER HERE'
+AD_SERVER = os.environ['AD_SERVER']
